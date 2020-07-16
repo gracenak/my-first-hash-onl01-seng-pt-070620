205ae7795dd6c105a803129b94e9f64164aaea90
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-16 04:27:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,8 +17,6 @@ def retrieval
                  }
 
   shipping_manifest["oil paintings"] 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
@@ -27,11 +25,11 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["muskets"] = 2# add 2 muskets to the shipping_manifest hash below
 
 
 
-  # add 4 gun powder to the shipping_manifest hash below
+  shipping_manifest["gun powder"] = 4# add 4 gun powder to the shipping_manifest hash below
 
 
 
